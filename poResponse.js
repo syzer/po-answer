277411e8c7931253914ae758ce77e0ecd8e1aeea
syzer3@gmail.com <syzer3@gmail.com>
2016-10-14 20:19:48 +0200

    seven

--- poResponse.js
+++ poResponse.js
@@ -2,9 +2,12 @@
 
 const _ = require('lodash')
 const tap = _.curryRight(_.tap)
+const ask = tap(_.flow(_.curry(_.first), console.log))
 
-module.exports = _.flow(tap(() => console.log('- Can we do it?')), _.sample, console.log)([
-    'to specific', 'not stable enough', 'requires a lot of thinking first', 'to much traffic', 'not specific enough', 'this is not what we do',
-    'i\'ll try to sell this idea to a client', 'hopefully client will buy it', 'do not fully understand the benefit', 'i want all team focus on that subject',
-    'we don\'t have expertise in that area'
+module.exports = _.flow(ask,  _.sample, console.log)([
+    '- Can we do it?',
+    'to specific', 'not stable enough', 'requires a lot of thinking first', 'to much traffic',
+    'not specific enough', 'this is not what we do', 'i\'ll try to sell this idea to a client',
+    'hopefully client will buy it', 'do not fully understand the benefit',
+    'i want all team focus on that subject', 'we don\'t have expertise in that area'
 ])
